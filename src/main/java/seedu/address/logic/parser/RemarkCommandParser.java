26701bcef7e4a869b834f914ade5a925b4fe5583
chinzhening <ning.chin03@gmail.com>
2024-03-13 10:26:04 +0800

    Add Remark to AddressBook following the AddRemark tutorial
    Code used was largely adapted from the tutorial. JUnit tests for
    `RemarkCommand` have been added.

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses the remark command
+ * Adapted from: @see <a href="https://nus-cs2103-ay2324s2.github.io/tp/tutorials/AddRemark.html">AddRemark tutorial</a>
+ */
+public class RemarkCommandParser implements Parser {
+    public static final Prefix PREFIX_REMARK = new Prefix("r/");
+    @Override
+    public RemarkCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                            RemarkCommand.MESSAGE_USAGE), ive);
+        }
+
+        Remark remark = new Remark(argMultimap.getValue(PREFIX_REMARK).orElse(""));
+
+        return new RemarkCommand(index, remark);
+    }
+}
